544e162e55f3bf148252e82a6db44c95abfb450d
mlopezo2 <160878713+mlopezo2@users.noreply.github.com>
2024-02-22 10:52:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from gtts import gTTS
 from PIL import Image
 
 st.title("Interfases Multimodales.")
-image = Image.open('text_to_audio.png')
+image = Image.open('Babyyoda.jpg')
 
 st.image(image, width=200)
 
